cc5b44f741f5cd8fd00aa5e2a108c2419fd439d3
Sergiy Ignatenko <grydinywka@gmail.com>
2016-03-20 21:33:01 +0200

    update urls

--- caesar/urls.py
+++ caesar/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-# from caesarapp.views import index
+from caesarapp import views
 
 urlpatterns = [
     # index page
-    url(r'^$', 'caesarapp.views.index', name='index'),
+    url(r'^$', views.index, name='index'),
 
     # admin page
     url(r'^admin/', include(admin.site.urls)),
