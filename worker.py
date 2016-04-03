5baac1a551a6f6286f50b04440825d93f70d273c
Sergiy Ignatenko <grydinywka@gmail.com>
2016-04-03 23:07:06 +0300

    mod path in worker 3

--- worker.py
+++ worker.py
@@ -5,7 +5,6 @@ redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
-    # with open('./caesarapp/static/txt/words.txt', 'r') as en_words_file:
-    #     for line in en_words_file:
-    #         conn.sadd('setEn', line.strip())
-    conn.flushall()
+    with open('./caesarapp/static/txt/words.txt', 'r') as en_words_file:
+        for line in en_words_file:
+            conn.sadd('setEn', line.strip())
