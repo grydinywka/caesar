4e334c519c983a4823f6267553e1fdca43fc50d2
Sergiy Ignatenko <grydinywka@gmail.com>
2016-04-03 16:55:32 +0300

    modify worker

--- worker.py
+++ worker.py
@@ -3,13 +3,15 @@ import os
 import redis
 from rq import Worker, Queue, Connection
 
-listen = ['high', 'default', 'low']
+# listen = ['high', 'default', 'low']
 
 redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
-
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
-    with Connection(conn):
-        worker = Worker(map(Queue, listen))
-        worker.work()
+    # with Connection(conn):
+    #     worker = Worker(map(Queue, listen))
+    #     worker.work()
+    with open('./caesar/caesarapp/static/txt/wordsEn.txt', 'r') as en_words_file:
+        for line in en_words_file:
+            conn.sadd('setEn', line.strip())
